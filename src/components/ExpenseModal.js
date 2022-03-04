a517de82c72e66c81830f3a384003e5be289160b
Alp Tarla <alptarla@gmail.com>
2022-03-04 17:32:04 +0300

    Wrap modal title to ModalTitle component

--- src/components/ExpenseModal.js
+++ src/components/ExpenseModal.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Modal } from 'react-bootstrap'
 import NewExpenseForm from './NewExpenseForm'
 
-function ExpenseModal({ show, onHide, onAdd, defaultBudget }) {
+function ExpenseModal({ show, onHide, onAdd, budget }) {
   return (
     <Modal show={show} onHide={onHide}>
-      <Modal.Header closeButton>New Expense</Modal.Header>
+      <Modal.Header closeButton>
+        <Modal.Title>New Expense</Modal.Title>
+      </Modal.Header>
       <Modal.Body>
-        <NewExpenseForm onSubmit={onAdd} defaultBudget={defaultBudget} />
+        <NewExpenseForm onSubmit={onAdd} budget={budget} />
       </Modal.Body>
     </Modal>
   )
