5a544f5ac7f24f845a827ca43e538d3f229770bb
Alp Tarla <alptarla@gmail.com>
2022-03-06 15:17:40 +0300

    Move currency formatter util function to helpers folder

--- src/components/BudgetPrice.js
+++ src/components/BudgetPrice.js
@@ -1,18 +1,14 @@
 import React from 'react'
+import { formatCurrency } from '../helpers/currency'
 
 function BudgetPrice({ amount, max }) {
-  const priceFormatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  })
-
   return (
     <div>
-      <span>{priceFormatter.format(amount)}</span>
+      <span>{formatCurrency(amount)}</span>
       {max && (
         <>
           <span className="mx-1">/</span>
-          <small className="text-muted">{priceFormatter.format(max)}</small>
+          <small className="text-muted">{formatCurrency(max)}</small>
         </>
       )}
     </div>
