af06690dfa3b5b69b0957d245278fbacb0feb6ba
Alp Tarla <alptarla@gmail.com>
2022-03-06 15:37:46 +0300

    Test to ExpenseListModal

---
+++ src/__tests__/unit/ExpenseListModal.spec.js
@@ -0,0 +1,22 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import ExpenseListModal from '../../components/ExpenseListModal'
+import { testBudget } from './BudgetCard.spec'
+
+describe('<ExpenseListModal />', () => {
+  test('renders correctly', () => {
+    render(<ExpenseListModal show={true} budget={testBudget} />)
+    expect(screen.getByText(/expenses - /i)).toBeInTheDocument()
+  })
+
+  test('should be call close button', () => {
+    const onHideFn = jest.fn()
+    render(
+      <ExpenseListModal show={true} budget={testBudget} onHide={onHideFn} />
+    )
+
+    const closeButton = screen.getByLabelText(/close/i)
+    fireEvent.click(closeButton)
+
+    expect(onHideFn).toBeCalled()
+  })
+})
